4c37597e3a4a2817128ce2a6c4573667a5a32b0f
Nicolas Resende <nicolascampos1093@gmail.com>
2022-01-05 12:08:11 -0300

    pause for now

--- components/Style.jsx
+++ components/Style.jsx
@@ -3,10 +3,14 @@ export default function Style(props) {
         <div>
             <h1 style={ {
                 backgroundColor: props.num >= 0 ? "yellow" : "blueviolet",
-                color: props.color
+                color: props.color,
+                textAlign: props.right ? "right" : "left",
                 } }>
                 Text
             </h1>
+            <h2 className={props.num >= 0 ? "crimson" : "dodgerblue"}>
+                Text
+            </h2>
         </div>
     )
 }
--- pages/css/usingstyle.jsx
+++ pages/css/usingstyle.jsx
@@ -3,7 +3,7 @@ import Style from "../../components/Style"
 export default function usingstyle() {
     return(
         <>
-            <Style num={42} color="black"></Style>
+            <Style num={42} color="black" right></Style>
             <Style num={-42} color="white"></Style>
         </>
     )
